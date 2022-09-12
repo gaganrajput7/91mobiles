4d2f49c91ac7172f4133fed881814eaa8c1f1c4a
gaganrajput7 <gaganrajputbapora@gmail.com>
2022-09-12 12:50:26 +0530

    commit

--- backend/db/db.js
+++ backend/db/db.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const db =
-  process.env.DATABASE ||
-  "mongodb+srv://gaganrajput875:gaganrajput@cluster0.ntnneor.mongodb.net/91mobiles?retryWrites=true&w=majority";
+  process.env.DATABASE
 const connection = mongoose.connect(db, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
